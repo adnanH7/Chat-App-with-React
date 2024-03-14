4e2740a28138f53fe2b4215ae4e450d2a7a8a9f0
adnanH7 <adnankhan17371@gmail.com>
2024-03-14 05:51:33 +0530

    React first Project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
+//import ChatFeed from './components/ChatFeed';
+//import LoginForm from './components/LoginForm';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <ChatEngine
+            height="100vh"
+            projectID="589d6456-a8c8-4835-ab58-d5a492da976d"
+            userName="adnan"
+            userSecret="123"
+          //  renderChatFeed={(chatAppProps)=> <ChatFeed {...chatAppProps}/>}
+        />
+    )
 }
-
 export default App;
